6400d1858e1686ac9c2e21471df3876839ec18e0
ivan <bernini@inventati.org>
2021-05-18 22:00:57 +0200

    ui

--- lib/web/components/sidebar_navigation_live.ex
+++ lib/web/components/sidebar_navigation_live.ex
@@ -1,4 +1,8 @@
 defmodule Bonfire.UI.Reflow.SidebarNavigationLive do
-  use Bonfire.Web, :live_component
+  use Bonfire.Web, :stateless_component
+  alias Surface.Components.LivePatch
+
+  prop page, :string, required: true
+  prop username, :string, required: true
 
 end
