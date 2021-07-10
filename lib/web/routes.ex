bca70a469feee3106d0b33ee4c98da6e2e59f26c
Mayel <git@mayel.space>
2021-07-10 13:33:25 +0200

    routes

--- lib/web/routes.ex
+++ lib/web/routes.ex
@@ -6,9 +6,9 @@ defmodule Bonfire.UI.Reflow.Routes do
       # pages anyone can view
       scope "/", Bonfire.UI.Reflow do
         pipe_through :browser
-        live "/resource/:id", ResourceLive
+        live "/resource/:id", ResourceLive, as: ValueFlows.EconomicResource
         live "/resource/:id/:tab", ResourceLive
-        live "/process/:id", ProcessLive
+        live "/process/:id", ProcessLive, as: ValueFlows.Process
         live "/process/:id/:tab", ProcessLive
         live "/processes", ProcessesLive
         live "/materials", MaterialsLive
