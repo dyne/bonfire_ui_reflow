047075a315ba12dcb4ad740d56cb4680277a3062
Mayel de Borniol <git@mayel.space>
2022-03-10 18:29:17 +1300

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -7,15 +7,5 @@ defmodule Bonfire.UI.Reflow.Test.FakeHelpers do
 
   import Bonfire.UI.Reflow.Integration
 
-  def fake_account!(attrs \\ %{}) do
-    cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = repo().insert(cs)
-    account
-  end
-
-  def fake_user!(%Account{}=account, attrs \\ %{}) do
-    assert {:ok, user} = Users.create(Fake.user(attrs), account)
-    user
-  end
 
 end
