f49f11fc1734d3b49a43c3954e5ffa466dfa6222
Mayel de Borniol <git@mayel.space>
2022-03-17 12:36:38 +1300

    Clean tests

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -38,11 +38,7 @@ defmodule Bonfire.UI.Reflow.ConnCase do
 
     import Bonfire.UI.Reflow.Integration
 
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(repo())
-
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(repo(), {:shared, self()})
-    end
+    Bonfire.Common.Test.Interactive.setup_test_repo(tags)
 
     {:ok, []}
   end
