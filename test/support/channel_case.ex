308bd93e7c48aa2cc91d8a416e76c76adde77544
Mayel de Borniol <git@mayel.space>
2022-06-14 09:01:46 +1200

    Use compile_env

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -26,7 +26,7 @@ defmodule Bonfire.UI.Reflow.ChannelCase do
       import Bonfire.UI.Reflow.ChannelCase
 
       # The default endpoint for testing
-      @endpoint Bonfire.Common.Config.get!(:endpoint_module)
+      @endpoint Application.compile_env!(:bonfire, :endpoint_module)
     end
   end
 
