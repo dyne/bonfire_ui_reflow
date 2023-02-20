e7ec02641d3f0f3837c18352714ed1a52c037c18
Mayel de Borniol <git@mayel.space>
2023-02-20 13:58:48 +1300

    moduledoc

--- priv/repo/migrations/20200523081012_init_pointers.exs
+++ priv/repo/migrations/20200523081012_init_pointers.exs
@@ -1,4 +1,5 @@
-defmodule Bonfire.UI.Reflow.Repo.Migrations.InitPointers do
+defmodule Bonfire.UI.Reflow.Repo.Migrations.InitPointers  do
+  @moduledoc false
   use Ecto.Migration
   import Pointers.Migration
   import Pointers.ULID.Migration
--- priv/repo/migrations/20201105094943_import_me.exs
+++ priv/repo/migrations/20201105094943_import_me.exs
@@ -1,4 +1,5 @@
-defmodule Bonfire.UI.Reflow.Repo.Migrations.ImportMe do
+defmodule Bonfire.UI.Reflow.Repo.Migrations.ImportMe  do
+  @moduledoc false
   use Ecto.Migration
 
   import Bonfire.UI.Reflow.Migration
